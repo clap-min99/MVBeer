45254062b1a5b1b63a31aa260d4e93879bf59fbb
clap_min99 <qkrtnals032@gmail.com>
2024-11-18 12:44:52 +0900

    api

--- back/movies/views.py
+++ back/movies/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from django.conf import settings
+import requests
+
 
 # Create your views here.
-## 
+
+@api_view(['GET'])
+def index(request):
+    api_key = settings.API_KEY
+
+    url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzQ3OWE3OWYxMmI0MzIxMDJiMWNkYTA2ZjY5NWY4NiIsIm5iZiI6MTczMTg5OTQxNC4zODg3NjEzLCJzdWIiOiI2NzJkYmVkODliN2UwOWNmZGNjNDQ4ZjMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.n3kNqTXyXaOSDePDuje8haUvwaDhgzh9O6weI061pQQ"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return Response(response)
