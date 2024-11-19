b24eb6e6e659ea76b93c3e326b3ec7529412d911
clap_min99 <qkrtnals032@gmail.com>
2024-11-19 17:54:26 +0900

    ch

--- front/mov_eer/src/views/MovieDetailView.vue
+++ front/mov_eer/src/views/MovieDetailView.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
+    <nav>
+        <RouterLink :to="{name: 'MainView'}">Main</RouterLink>
+    </nav>
+        
+
 
     </div>
 </template>
 
 <script setup>
+import { RouterView, RouterLink } from 'vue-router'
+
+
 
 </script>
 
