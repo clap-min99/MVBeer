6315b5bde1bb53f700ce63f3e61879f7590f46a5
clap_min99 <qkrtnals032@gmail.com>
2024-11-20 17:20:54 +0900

    login

--- front/mov_eer/src/router/index.js
+++ front/mov_eer/src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '@/views/MainView.vue'
 import MovieDetailView from '@/views/MovieDetailView.vue'
 import AlcoholView from '@/views/AlcoholView.vue'
-
+import SignUpView from '@/views/SignUpView.vue'
+import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -21,8 +22,17 @@ const router = createRouter({
       name: 'AlcoholView',
       component: AlcoholView
     },
+    {
+      path: '/signup',
+      name: 'SignUpView',
+      component: SignUpView
+    },
+    {
+      path: '/login',
+      name: 'LoginView',
+      component: LoginView
+    }
   ],
-  
 })
 
 export default router
