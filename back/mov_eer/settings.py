2a9435ad6816301a5a350fcfd4619f9417410321
dmswjdsz <eunjung5406@naver.com>
2024-11-20 12:44:32 +0900

    picture

--- back/mov_eer/settings.py
+++ back/mov_eer/settings.py
@@ -124,15 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Imgur API Key
-IMGUR_CLIENT_ID = env('IMGUR_CLIENT_ID')
-IMGUR_CLIENT_SECRET = env('IMGUR_CLIENT_SECRET')
-IMGUR_API_URL = "https://api.imgur.com/3/upload"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- back/movies/urls.py
+++ back/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-from .views import upload_image
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('movies/', views.movie_all),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('beverages/', views.beverage_main),
     path('beers/', views.beer_list)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
